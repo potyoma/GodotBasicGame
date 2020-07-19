37d8299ecaf41f97c3e24394f8f366ac28070fc6
potyoma <artyomnovosyolov254@gmail.com>
2020-07-19 22:49:08 +0300

    tryed to make Sprite2 rotate around Sprite

---
+++ Game/SpriteChildRotation.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+using static System.Math;
+
+public class SpriteChildRotation : Sprite
+{
+    bool isPlaying = true;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        do
+        {
+            this.Position += new Vector2(10, 10);
+        }
+        while (isPlaying);
+    }
+}
