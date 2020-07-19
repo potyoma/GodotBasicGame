6a5e3453f356f297725bb40497bf4ba3da240822
potyoma <artyomnovosyolov254@gmail.com>
2020-07-19 17:53:25 +0300

    added moving up to a script

--- Game/Sprite.cs
+++ Game/Sprite.cs
@@ -3,19 +3,20 @@ using System;
 
 public class Sprite : Godot.Sprite
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        float amount = 5;
+        if (Input.IsKeyPressed((int)KeyList.W))
+        {
+            this.Position += new Vector2(0, -amount);
+        }
+    }
 }
